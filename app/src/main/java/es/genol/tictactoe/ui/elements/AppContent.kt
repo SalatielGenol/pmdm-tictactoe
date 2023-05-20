9521cae0f1d8cb3cb88ee8507edc855d76e42064
SalatielGenol <salatiel@genol.es>
2023-05-20 02:00:24 +0200

    Optimize imports

--- app/src/main/java/es/genol/tictactoe/ui/elements/AppContent.kt
+++ app/src/main/java/es/genol/tictactoe/ui/elements/AppContent.kt
@@ -1,19 +1,11 @@
 package es.genol.tictactoe.ui.elements
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -22,12 +14,9 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import es.genol.tictactoe.ui.state.GameState
-import es.genol.tictactoe.ui.theme.CrossIcon
-import es.genol.tictactoe.ui.theme.DrawIcon
 import es.genol.tictactoe.ui.theme.TicTacToeTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
